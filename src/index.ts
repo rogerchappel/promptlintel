4348152f0f9a0c4840bb3d95fa742a21a3e9a1d3
Roger Chappel <miscanalysis@gmail.com>
2026-05-11 06:36:35 +1000

    feat: expose library API

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export { scan } from './scan.js';
+export { defaultRules } from './rules.js';
+export { lintText } from './engine.js';
+export { renderJson, renderMarkdown } from './reporters.js';
+export type { Finding, LintConfig, Rule, ScanOptions, ScanReport, Severity } from './types.js';
